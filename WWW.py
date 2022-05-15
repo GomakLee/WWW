94ae8d833c2551e3ca7066ac5af15a0b91b8800c
Mosogomak <102731508+GodSaeng@users.noreply.github.com>
2022-05-15 14:23:08 +0900

    새로운 텍스트 열어서 거기에 웹사이트 소스코드 추가

--- WWW.py
+++ WWW.py
@@ -1,2 +1,14 @@
 #실시간 검색어 크롤링
 import requests
+
+url = "https://zum.com/"
+response = requests.get(url)
+print(response)
+#print(response.text)
+response_text = response.text
+
+
+f = open("Text.txt","w", encoding='utf-8')
+
+f.write(response_text)
+f.close()
