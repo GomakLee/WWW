318d471bcddcb9a5f661e5331861874345e95236
Mosogomak <102731508+GodSaeng@users.noreply.github.com>
2022-05-16 17:09:46 +0900

    일부

--- WWW.py
+++ WWW.py
@@ -1,14 +1,18 @@
 #실시간 검색어 크롤링
 import requests
-
+from bs4 import BeautifulSoup
 url = "https://zum.com/"
 response = requests.get(url)
-print(response)
-#print(response.text)
-response_text = response.text
-
-
+#print(response)
+source_text = response.text
+'''
+soup = BeautifulSoup(source_text, "html.parser")
+#print(soup)
+#print(type(soup))
+only_need = soup.findAll("div.issue_cont new")
+print(only_need)
 f = open("Text.txt","w", encoding='utf-8')
-
-f.write(response_text)
-f.close()
+for i in only_need:
+    f.write(i)
+f.close()'''
+print(response.status_code)
