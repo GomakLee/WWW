7ad9f88ab1fc6950d39a8bbcdd29d04c760ffdda
Mosogomak <102731508+GodSaeng@users.noreply.github.com>
2022-05-18 17:04:14 +0900

    왜 난 파이썬 실시간 검색어 크롤링 하면 왜 도대체 왜 절반만 나오는거지??아 당최 이해할 수 가없네

--- WWW.py
+++ WWW.py
@@ -1,18 +1,16 @@
 #실시간 검색어 크롤링
+from cgitb import text
 import requests
 from bs4 import BeautifulSoup
 url = "https://zum.com/"
 response = requests.get(url)
 #print(response)
 source_text = response.text
-'''
+
 soup = BeautifulSoup(source_text, "html.parser")
 #print(soup)
 #print(type(soup))
-only_need = soup.findAll("div.issue_cont new")
-print(only_need)
-f = open("Text.txt","w", encoding='utf-8')
-for i in only_need:
-    f.write(i)
-f.close()'''
-print(response.status_code)
+
+#only_need = soup.select("#app > div.wrap > header > div.gnb_wrap > div.gnb_menu_wrap > div.w_inner > div.issue_wrap > div.issue_cont new >div.list_wrap issue_type")
+text = soup.findAll("span","inner_txt")
+print(text)
